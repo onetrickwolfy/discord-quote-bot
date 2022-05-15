e3b3e2ecec25aa6c014f1d7acddb45b3f61f2592
Gabey <fluffygabey@gmail.com>
2022-05-15 19:53:14 +0200

    cleanup before moving to hikari

--- discord-quote-bot/app.py
+++ discord-quote-bot/app.py
@@ -1,10 +1,8 @@
 from utils import *
 
-from nextcord.ext import commands
 from os import listdir, getenv
 from yaml import safe_load
 import logging
-import cogs
 
 
 # LOADING THE CONFIGURATION FILE
@@ -20,16 +18,3 @@ token = getenv('discord-token') if getenv('discord-token') else conf['token']
 init_logger(logger)
 
 
-# DISCORD BOT SET-UP
-client = commands.Bot()
-
-extentions = []
-
-for folder in listdir('cogs'):
-    for file in listdir(f'cogs/{folder}'):
-        if file.endswith('.py'):
-            extentions.append(f'cogs.{folder}.{file[:-3]}')
-
-[client.load_extension(extention) for extention in extentions]
-
-client.run(token)
