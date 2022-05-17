ed8e85bcf2ec508eea178f1c38923aa80d33bc19
Gabey <fluffygabey@gmail.com>
2022-05-17 20:45:51 +0200

    quote - draft

---
+++ discord-quote-bot/extensions/quotes.py
@@ -0,0 +1,54 @@
+import hikari
+import lightbulb
+
+plugin = lightbulb.Plugin("Quote-Maker")
+
+# add @lightbulb.add_cooldown(15.0, 1,
+# lightbulb.SlashCommandCompletionEvent) later
+
+
+# TODO: Implement the check (author must not be @everyone or @here)
+@lightbulb.Check
+def check_author(ctx: lightbulb.Context) -> bool:
+    return True
+
+
+@plugin.command
+@lightbulb.command("quote", "Click to quote this message.")
+@lightbulb.implements(lightbulb.MessageCommand)
+async def make_quote(ctx: lightbulb.Context) -> None:
+    await ctx.respond('test')
+
+
+@plugin.command
+@lightbulb.command("test", "Click to quote this message.")
+@lightbulb.implements(lightbulb.SlashCommand)
+async def make_quote(ctx: lightbulb.Context) -> None:
+    await ctx.respond(ctx.get_guild().owner_id)
+
+
+@plugin.command
+@lightbulb.add_checks(check_author)
+@lightbulb.option('author', 'The author of the quote',
+                  required=True, default='anoymous-wolf', choices=[])
+@lightbulb.option('quote', 'The text you would like to quote', required=True)
+@lightbulb.command("quote_this", "Generates a quote when invoked.")
+@lightbulb.implements(lightbulb.SlashCommand)
+async def make_quote(ctx: lightbulb.Context) -> None:
+    await ctx.respond('test')
+
+
+@plugin.command
+@lightbulb.option('quote', 'The text you would like to quote', required=True)
+@lightbulb.command("quote_me", "Generates a quote when invoked.")
+@lightbulb.implements(lightbulb.SlashCommand)
+async def make_quote(ctx: lightbulb.Context) -> None:
+    await ctx.respond('pong! uwu')
+
+
+def load(bot: lightbulb.BotApp) -> None:
+    bot.add_plugin(plugin)
+
+
+def unload(bot: lightbulb.BotApp) -> None:
+    bot.remove_plugin(plugin)
