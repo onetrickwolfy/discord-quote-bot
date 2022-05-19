9630a693be976a9310c0a340da57d435ff75ac58
Gabey <fluffygabey@gmail.com>
2022-05-19 02:23:17 +0200

    grouped owner check and authorized commands in only one guild

--- discord-quote-bot/extensions/bot_administration.py
+++ discord-quote-bot/extensions/bot_administration.py
@@ -2,42 +2,39 @@ import hikari
 import lightbulb
 
 plugin = lightbulb.Plugin("Bot-Administration")
-
+guilds = [964818125503750174]
+plugin.add_checks(lightbulb.owner_only)
 
 # -----------------------------------------------------
 
 
 @plugin.command
-@lightbulb.add_checks(lightbulb.owner_only)
-@lightbulb.command("reload_extension", "reload extensions", hidden=True)
-@lightbulb.implements(lightbulb.PrefixCommand)
+@lightbulb.command("reload_extension", "reload extensions", hidden=True, guilds=guilds)
+@lightbulb.implements(lightbulb.SlashCommand)
 async def reload_extension(ctx: lightbulb.Context):
     [ctx.bot.reload_extensions(ext) for ext in ctx.bot.extensions]
     await ctx.respond("Reloaded all extensions successfully. ;p", reply=False)
 
 
 @plugin.command
-@lightbulb.add_checks(lightbulb.owner_only)
 @lightbulb.command("sync_application_commands",
-                   "sync the accplication commands", hidden=True)
-@lightbulb.implements(lightbulb.PrefixCommand)
+                   "sync the accplication commands", hidden=True, guilds=guilds)
+@lightbulb.implements(lightbulb.SlashCommand)
 async def sync_application_commands(ctx: lightbulb.Context):
     await ctx.bot.sync_application_commands()
     await ctx.respond("Synced application commands", reply=False)
 
 
 @plugin.command
-@lightbulb.add_checks(lightbulb.owner_only)
-@lightbulb.command("get_latency", "send the current latency", hidden=True)
-@lightbulb.implements(lightbulb.PrefixCommand)
+@lightbulb.command("get_latency", "send the current latency", hidden=True, guilds=guilds)
+@lightbulb.implements(lightbulb.SlashCommand)
 async def get_latency(ctx: lightbulb.Context):
     await ctx.respond(ctx.bot.heartbeat_latency, reply=False)
 
 
 @plugin.command
-@lightbulb.add_checks(lightbulb.owner_only)
-@lightbulb.command("purge_commands", "purge the commands", hidden=True)
-@lightbulb.implements(lightbulb.PrefixCommand)
+@lightbulb.command("purge_commands", "purge the commands", hidden=True, guilds=guilds)
+@lightbulb.implements(lightbulb.SlashCommand)
 async def purge_commands(ctx: lightbulb.Context):
     await ctx.bot.purge_application_commands(global_commands=True)
     await ctx.respond("Purged application commands", reply=False)
