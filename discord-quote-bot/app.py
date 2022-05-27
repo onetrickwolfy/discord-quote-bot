69f1f211b2fee70e76c6a1029f6d70284b6183fe
Gabey <fluffygabey@gmail.com>
2022-05-27 21:17:39 +0200

    fixed bot

--- discord-quote-bot/app.py
+++ discord-quote-bot/app.py
@@ -28,43 +28,45 @@ init_logger(logger)
 # -----------------------------------------------------
 default_enabled_guilds = (964818125503750174)
 
-if __name__ == "__main__":
 
+bot = lightbulb.BotApp(
+    token=token,
+    prefix='>',
+    intents=hikari.Intents.ALL,
+    delete_unbound_commands=True,
+    default_enabled_guilds=default_enabled_guilds
+)
+
+bot.load_extensions_from("./extensions/", must_exist=True)
+
+@bot.listen()
+async def on_guild_join(event: hikari.GuildJoinEvent) -> None:
+    guilds_settings.upsert(
+        {
+            'guild_id': event.guild_id,
+            'global': True,
+            'hall_of_fame': None
+        },
+        Query().guild_id == event.guild_id
+    )
+
+@bot.listen()
+async def on_guild_leave(event: hikari.GuildLeaveEvent) -> None:
+    guilds_settings.remove(Query().guild_id == event.guild_id)
+
+@bot.listen()
+async def on_starting(event: hikari.StartingEvent) -> None:
+    bot.d.loop = asyncio.get_running_loop()
+    bot.d.aio_session = aiohttp.ClientSession()
+    bot.d.process_pool = concurrent.futures.ProcessPoolExecutor()
+
+
+
+if __name__ == "__main__":
     if os_name != "nt":
         import uvloop  # type: ignore (uvloop does not exist on windows.)
         uvloop.install()
-
-    bot = lightbulb.BotApp(
-        token=token,
-        prefix='>',
-        intents=hikari.Intents.ALL,
-        delete_unbound_commands=True,
-        default_enabled_guilds=default_enabled_guilds
-    )
-
-    bot.load_extensions_from("./extensions/", must_exist=True)
-
-    @bot.listen()
-    async def on_guild_join(event: hikari.GuildJoinEvent) -> None:
-        guilds_settings.upsert(
-            {
-                'guild_id': event.guild_id,
-                'global': True,
-                'hall_of_fame': None
-            },
-            Query().guild_id == event.guild_id
-        )
-
-    @bot.listen()
-    async def on_guild_leave(event: hikari.GuildLeaveEvent) -> None:
-        guilds_settings.remove(Query().guild_id == event.guild_id)
-
-    @bot.listen()
-    async def on_starting(event: hikari.StartingEvent) -> None:
-        bot.d.loop = asyncio.get_running_loop()
-        bot.d.aio_session = aiohttp.ClientSession()
-        bot.d.process_pool = concurrent.futures.ProcessPoolExecutor()
-
+        
     bot.run(
         status=hikari.Status.ONLINE,
         activity=hikari.Activity(
